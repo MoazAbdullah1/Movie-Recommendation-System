41fb477434cfe6d15dfc57d3592896be21f26253
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-07-31 14:12:36 +0500

    MOVIE RECOMMEMDATION SYSTEM

---
+++ Scripts.js
@@ -0,0 +1,22 @@
+
+const payButton = document.getElementById("payButton");
+const bankDetailsForm = document.getElementById("bankDetailsForm");
+
+payButton.addEventListener("click", () => {
+    // Show the bank details form when the Pay button is clicked
+    bankDetailsForm.style.display = "block";
+});
+
+const bankForm = document.getElementById("bankForm");
+bankForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Here you can write code to process the form data,
+    // send it to the server, and handle the payment.
+
+    // After processing the payment, you might want to show
+    // a success message or redirect the user to a thank-you page.
+    // For demonstration purposes, we'll simply hide the form.
+    bankDetailsForm.style.display = "none";
+});
+
